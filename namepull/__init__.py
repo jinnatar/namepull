b8c96b97c6c8e4dfc721501cf6e5244c20475640
Artanicus <artanicus@nocturnal.fi>
2021-08-08 21:28:07 +0300

    Reformat code with yapf

--- namepull/__init__.py
+++ namepull/__init__.py
@@ -19,7 +19,9 @@ flags.DEFINE_string('db_host', '127.0.0.1', 'MAD DB host')
 flags.DEFINE_string('db_pass', None, 'MAD DB password')
 flags.DEFINE_string('db_user', None, 'MAD DB user')
 flags.DEFINE_integer('batchsize', '5', 'Queries per request')
-flags.DEFINE_integer('loop_interval', None, 'Interval in hours to poll repeatedly. A random 25% jitter is applied.')
+flags.DEFINE_integer(
+    'loop_interval', None,
+    'Interval in hours to poll repeatedly. A random 25% jitter is applied.')
 
 # API endpoint & token must be provided as flags
 flags.mark_flag_as_required("token")
@@ -35,24 +37,31 @@ def batch(iterable, n=1):
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
 
+
 # Return a delay with a random percentage jitter added
 def pct_jitter(value, multiplier):
     jitter = backoff.full_jitter(value * multiplier)
     return value + jitter
 
+
 # Async json data downloader
-@backoff.on_exception(backoff.expo, aiohttp.ClientError, max_time=60, logger='absl')
+@backoff.on_exception(backoff.expo,
+                      aiohttp.ClientError,
+                      max_time=60,
+                      logger='absl')
 async def download(url, data, session, method='POST', **kwargs) -> str:
     response = await session.request(method=method, url=url, json=data)
     response.raise_for_status()
     data = await response.json()
     return data
 
+
 # download data -> store data
 async def get_gyms(ids, **kwargs) -> None:
     url = '{api}?token={token}'.format(api=FLAGS.api, token=FLAGS.token)
     response = await download(url, ids, **kwargs)
     await store_gyms(response, **kwargs)
+
 
 # Process received data into UPDATE statements & execute them
 async def store_gyms(data, cursor, table, **kwargs) -> None:
@@ -67,6 +76,7 @@ async def store_gyms(data, cursor, table, **kwargs) -> None:
                               columns=columns,
                               values=values)
         cursor.execute(*update)
+
 
 # Retrieve unknown gyms, resolve them & store them
 async def get_unknown_gyms():
@@ -91,10 +101,11 @@ async def get_unknown_gyms():
         async with ClientSession() as session:
             tasks = []
             for i in batch(ids, FLAGS.batchsize):
-                tasks.append(get_gyms(i, session=session, cursor=cursor,
-                                      table=gym))
+                tasks.append(
+                    get_gyms(i, session=session, cursor=cursor, table=gym))
             await asyncio.gather(*tasks)
         db.commit()
+
 
 # Repeatedly query for missing names on an interval in hours
 async def looper(interval):
@@ -104,8 +115,10 @@ async def looper(interval):
         logging.info('Querying now...')
         await get_unknown_gyms()
         interval_seconds = pct_jitter(interval_seconds, 0.05)
-        logging.info(f'Sleeping until next iteration in {interval_seconds:.2f}s')
+        logging.info(
+            f'Sleeping until next iteration in {interval_seconds:.2f}s')
         await asyncio.sleep(interval_seconds)
+
 
 # Wrap asyncio.run for easy compatibilty with absl.app
 def main(argv):
@@ -115,9 +128,11 @@ def main(argv):
     else:
         asyncio.run(get_unknown_gyms())
 
+
 # script endpoint installed by package
 def run():
     app.run(main)
+
 
 if __name__ == '__main__':
     run()
